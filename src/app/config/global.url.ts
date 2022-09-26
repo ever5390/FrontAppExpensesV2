2661923dee4572c63db38f03f87abb466f484c38
Ever Rosales Peña <everjrosalesp@gmail.com>
2022-09-26 11:43:04 -0500

    Se corrige host heroku del server

--- src/app/config/global.url.ts
+++ src/app/config/global.url.ts
@@ -1,3 +1,3 @@
 export const URL_BASE_HOST = "http://localhost:8082";
-//export const URL_BASE_HOST = "https://appexpensesback.rj.r.appspot.com";
+//export const URL_BASE_HOST = "https://app-manage-expense-nv.herokuapp.com";
 export const URL_BASE_API_V1 = URL_BASE_HOST + "/api/v1";
